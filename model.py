1b7de9aeae4d3ea0da0174634fc5827b7958b646
Kanishka Tyagi <kanishkaugee@gmail.com>
2026-07-15 13:39:48 -0700

    Step 36: reward_head_forward

--- model.py
+++ model.py
@@ -600,8 +600,14 @@ def format_preference(example):
         "rejected_text": rejected_text,
     }
 
-# Step 36 - reward_head_forward (not yet solved)
-# TODO: implement
+# Step 36 - reward_head_forward
+import torch
+
+def reward_head_forward(hidden_state, weight, bias):
+    """Map a final hidden state to a scalar reward via a linear projection."""
+    # TODO: project hidden_state (B, D) through weight (D,) plus scalar bias to get (B,) rewards
+    rewards = hidden_state @ weight.T + bias
+    return rewards.squeeze(-1)
 
 # Step 37 - pairwise_reward_loss (not yet solved)
 # TODO: implement
